07e9c2ee1213cf0c125c98c5c5682ee72d15c581
Dr. Gabor FACSKO <59848559+gfacsko@users.noreply.github.com>
2022-09-09 22:46:12 +0200

    HelloWorld

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+string line;
+
+Console.WriteLine("Szia, Vilag!"); // kiírás konzol képernyőre (a Console oszály statikus WriteLine metódusa)
+
+line = Console.ReadLine(); // Sor beolvasása
+Console.WriteLine(line);  // A beolvasott sor kiírása
+
+Console.Write(line);
+
+Console.WriteLine("Nyomj le egy billentyűt!");
+Console.ReadKey();
+       
+
