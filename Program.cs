cd6d56f2704881389b2427d36e8ddedfeee47cc9
Dr. Gabor FACSKO <59848559+gfacsko@users.noreply.github.com>
2022-09-19 01:04:54 +0200

    2022. szeptember 19.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,63 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string line;
+//string line;
 
-Console.WriteLine("Szia, Vilag!"); // kiírás konzol képernyőre (a Console oszály statikus WriteLine metódusa)
+//Console.WriteLine("Szia, Vilag!"); // kiírás konzol képernyőre (a Console oszály statikus WriteLine metódusa)
 
-line = Console.ReadLine(); // Sor beolvasása
+/*line = Console.ReadLine(); // Sor beolvasása
 Console.WriteLine(line);  // A beolvasott sor kiírása
 
 Console.Write(line);
 
 Console.WriteLine("Nyomj le egy billentyűt!");
-Console.ReadKey();
-       
+Console.ReadKey();*/
 
+/*
+int n = 0;
+byte b = 0;
+string str = "szoveg";
+char c = 'c';
+bool l = false;
+
+Console.WriteLine(n);
+Console.WriteLine(b);
+
+Console.WriteLine(str);
+Console.WriteLine(str[3]); // A szamozas 0-tol kezdodik.
+Console.WriteLine(c);
+
+Console.WriteLine(l);
+
+Console.WriteLine(sizeof(int)); // Wikipédia
+
+int j, k, m;
+
+j = 1;
+k = 2;
+//m = 3;
+
+Console.WriteLine(j);
+Console.WriteLine(k);
+//Console.WriteLine(m);
+
+//float j = 1.2;
+
+*/
+
+/*string str = "vege";
+
+Console.WriteLine("eleje, " + str);*/
+
+//int n = "szoveg";
+//int n = 1.4;
+
+int n = 137;
+
+Console.WriteLine(n % 42);
+
+Console.WriteLine(n == 43);
+Console.WriteLine(n > 43);
+Console.WriteLine(n < 43);
+Console.WriteLine(n <= 43);
+
+if (n > 42 && n == 137)
+    Console.WriteLine("Nagyobb");
