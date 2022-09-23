b7e59b3380756bb01fc680d1762e1378ff49f9aa
Dr. Gabor FACSKO <59848559+gfacsko@users.noreply.github.com>
2022-09-24 00:24:22 +0200

    Gyakorlás a 2022. szeptember 24-ei órára

--- Program.cs
+++ Program.cs
@@ -1,64 +1,145 @@
 ﻿// See https://aka.ms/new-console-template for more information
-/* Elso orai program
- * Developed by Gabor FACSKO (facsko.gabor@uni-milton.hu)
- * Milton Friedman University, Budapest, Hungary, 2022 
- */
+//string line;
+
+//Console.WriteLine("Szia, Vilag!"); // kiírás konzol képernyőre (a Console oszály statikus WriteLine metódusa)
+
+/*line = Console.ReadLine(); // Sor beolvasása
+Console.WriteLine(line);  // A beolvasott sor kiírása
+
+Console.Write(line);
+
+Console.WriteLine("Nyomj le egy billentyűt!");
+Console.ReadKey();*/
 
 /*
-string line = ""; // Ez a beolvasando sor szoveges valtozoja
+int n = 0;
+byte b = 0;
+string str = "szoveg";
+char c = 'c';
+bool l = false;
 
-Console.WriteLine("Szia, Vilag!"); // Kiiras*/
+Console.WriteLine(n);
+Console.WriteLine(b);
 
-// Pelda a ReadLine() metodusra
-/*Console.WriteLine("Irjon be egy szoveget ls nyomjon ENTER-t!");  // Kezelesi utasitas
-line = Console.ReadLine(); // Sor beolvasara
-//Console.WriteLine(line); // Teljesen felesleges barbarizmus (a beolvasott valtozo kiirasa) */
+Console.WriteLine(str);
+Console.WriteLine(str[3]); // A szamozas 0-tol kezdodik.
+Console.WriteLine(c);
 
-// Pelda ReadKey() metodusra
-/*Console.WriteLine("Nyomjon le egy billentyut!");  // Kezelesi utasitas
-Console.ReadKey();  // Beker egy billentyut */
+Console.WriteLine(l);
 
-// Pelda a Write() metodusra
-/*Console.Write("Teszt");
-Console.Write("Teszt2");*/
+Console.WriteLine(sizeof(int)); // Wikipédia
 
-/*int n; // n valtozo, egesz
+int j, k, m;
 
-n = 0;*/
+j = 1;
+k = 2;
+//m = 3;
 
-int n = 0;   // Egesz valtozo ertekadassal
-byte b = 0;  // byte ertekadassal
-
-// Egesz szam kiirasa
-Console.Write("Egész változó: "); Console.WriteLine(n);
-// Byte tipusu valtozo kiirasa
-Console.Write("Byte változó: "); Console.WriteLine(b);
-
-double r = 1.41; // Valos szam ertekadassal
-
-// Lebegopontos valtozo kiirasa
-Console.Write("Lebegőpontos változó: "); Console.WriteLine(r);
-
-char c = 't';  // Karakter
-string str = "Szöveg"; // Szoveg
-
-// Karakter kiirasa
-Console.Write("Karakter: "); Console.WriteLine(c);
-// String kiirasa
-Console.Write("Szoveg/string: "); Console.WriteLine(str);
-// A string egy elemenek kiirasa
-Console.Write("Karakter: "); Console.WriteLine(str[3]); 
-
-bool l, k;  // Logikai változok
-l = false;
-k = true;
-
-// Logikai valtozok kiirasa
-Console.Write("Boolean/logikai változók: "); 
-Console.Write(l);
-Console.Write(" ");
+Console.WriteLine(j);
 Console.WriteLine(k);
+//Console.WriteLine(m);
 
-// Pelda szoveg összefuzesere
-Console.Write("Szöveg változó: "); Console.WriteLine(str);
-Console.WriteLine(str + " még szöveg");
+//float j = 1.2;
+
+*/
+
+/*string str = "vege";
+
+Console.WriteLine("eleje, " + str);*/
+
+//int n = "szoveg";
+//int n = 1.4;
+
+//int n = 137;
+
+/*Console.WriteLine(n % 42);
+
+Console.WriteLine(n == 43);
+Console.WriteLine(n > 43);
+Console.WriteLine(n < 43);
+Console.WriteLine(n <= 43);
+
+if (n > 42 && n == 137)
+    Console.WriteLine("Nagyobb");*/
+
+/*for (int i=0;i<n;i++)
+    Console.WriteLine(i);*/
+
+/*
+// Tomb merete
+const int nSzavak = 10; 
+
+// Szavak tomb deklaracio
+string[] szavak = new string[nSzavak] {"egy","ketto","harom","negy","ot","hat","het","nyolc","kilenc","tiz"};
+
+// Kiiratas
+for (int i = 0; i < nSzavak;i++)
+    Console.WriteLine(szavak[i]);
+*/
+
+/*
+// Elso 100 szam kiiratasa ciklussal
+const int n = 100;
+
+for (int i = 1; i <= n; i++)
+    Console.WriteLine(i.ToString());
+*/
+
+/*
+ * 
+// Az elso n szam paros, paratlan és 13-mal oszthato számainak összege
+const int n = 100;
+
+// Összeg
+int s = 0;
+
+for (int i = 0; i <= n; i++)
+{
+    // Páros számok összege
+    //if ((i % 2) == 0)
+    // Páratlan számok összege
+    // if ((i % 2) == 1)
+    // 13-mal osztható számok összge
+    if ((i % 13) == 0)
+    {
+        s = s + i;
+    };
+};
+
+// Eredmeny kiiratasa
+Console.WriteLine(s);
+*/
+
+// A while ciklus
+
+const int n = 100;
+
+int i = 0;
+
+while (i < n)
+{
+    i++;
+    Console.WriteLine(i.ToString());
+}
+
+Console.WriteLine("While ciklus vége.");
+
+/*
+// A do-while ciklus
+
+const int n = 100;
+
+int i = 0;
+
+do
+{
+    i++;
+    Console.WriteLine(i.ToString());
+}
+while (i < n);
+
+
+Console.WriteLine("A do-while ciklus vége.");
+*/
+
+
