fb1cc2a0a540b849fd78a75bb0d34724c712e103
Dr. Gabor FACSKO <59848559+gfacsko@users.noreply.github.com>
2022-10-25 01:31:58 +0200

    Stringkezelés 2022. október 24.

--- Program.cs
+++ Program.cs
@@ -1,145 +1,135 @@
-﻿// See https://aka.ms/new-console-template for more information
-//string line;
-
-//Console.WriteLine("Szia, Vilag!"); // kiírás konzol képernyőre (a Console oszály statikus WriteLine metódusa)
-
-/*line = Console.ReadLine(); // Sor beolvasása
-Console.WriteLine(line);  // A beolvasott sor kiírása
-
-Console.Write(line);
-
-Console.WriteLine("Nyomj le egy billentyűt!");
-Console.ReadKey();*/
-
-/*
-int n = 0;
-byte b = 0;
-string str = "szoveg";
-char c = 'c';
-bool l = false;
-
-Console.WriteLine(n);
-Console.WriteLine(b);
-
-Console.WriteLine(str);
-Console.WriteLine(str[3]); // A szamozas 0-tol kezdodik.
-Console.WriteLine(c);
-
-Console.WriteLine(l);
-
-Console.WriteLine(sizeof(int)); // Wikipédia
-
-int j, k, m;
-
-j = 1;
-k = 2;
-//m = 3;
-
-Console.WriteLine(j);
-Console.WriteLine(k);
-//Console.WriteLine(m);
-
-//float j = 1.2;
-
-*/
-
-/*string str = "vege";
-
-Console.WriteLine("eleje, " + str);*/
-
-//int n = "szoveg";
-//int n = 1.4;
-
-//int n = 137;
-
-/*Console.WriteLine(n % 42);
-
-Console.WriteLine(n == 43);
-Console.WriteLine(n > 43);
-Console.WriteLine(n < 43);
-Console.WriteLine(n <= 43);
-
-if (n > 42 && n == 137)
-    Console.WriteLine("Nagyobb");*/
-
-/*for (int i=0;i<n;i++)
-    Console.WriteLine(i);*/
-
-/*
-// Tomb merete
-const int nSzavak = 10; 
-
-// Szavak tomb deklaracio
-string[] szavak = new string[nSzavak] {"egy","ketto","harom","negy","ot","hat","het","nyolc","kilenc","tiz"};
-
-// Kiiratas
-for (int i = 0; i < nSzavak;i++)
-    Console.WriteLine(szavak[i]);
-*/
-
-/*
-// Elso 100 szam kiiratasa ciklussal
-const int n = 100;
-
-for (int i = 1; i <= n; i++)
-    Console.WriteLine(i.ToString());
-*/
-
-/*
+﻿/*
+ * Stringkezelő programok
  * 
-// Az elso n szam paros, paratlan és 13-mal oszthato számainak összege
-const int n = 100;
+ * Developed by Gabor FACSKO (facsko.gabor@uni-milton.hu)
+ * Milton Friedman University, Budapest, Hungary, 2022 
+ * 
+ * */
 
-// Összeg
-int s = 0;
-
-for (int i = 0; i <= n; i++)
-{
-    // Páros számok összege
-    //if ((i % 2) == 0)
-    // Páratlan számok összege
-    // if ((i % 2) == 1)
-    // 13-mal osztható számok összge
-    if ((i % 13) == 0)
-    {
-        s = s + i;
-    };
-};
-
-// Eredmeny kiiratasa
-Console.WriteLine(s);
-*/
-
-// A while ciklus
-
-const int n = 100;
-
-int i = 0;
-
-while (i < n)
-{
-    i++;
-    Console.WriteLine(i.ToString());
-}
-
-Console.WriteLine("While ciklus vége.");
 
 /*
-// A do-while ciklus
-
-const int n = 100;
-
-int i = 0;
-
-do
-{
-    i++;
-    Console.WriteLine(i.ToString());
-}
-while (i < n);
-
-
-Console.WriteLine("A do-while ciklus vége.");
+ * Írjon programot, mely megszámolja, hogy a beírt mondatban hány darab "a" betű van!
 */
 
+/*
+// Bekérünk egy mondatot
+string str = "";
 
+Console.Write("Írjon be egy mondatot! ");
+str = Console.ReadLine();
+
+// A bekért mondat ellenőrzése
+Console.WriteLine("A beírt mondat: " + str);
+
+// Az "a" betűk megszámlálása
+// Az "a" betűk száma
+int nSample = 0;
+for (int i = 0; i < str.Length; i++) // A "Length" tulajdonság a string hossza
+{ 
+    if (str[i] == 'a')
+        nSample++; // nSample += 1; // nSample = nSample + 1;
+}
+
+// Az eredmény kiíratása
+Console.WriteLine("A beírt mondatban " + nSample + " a betű taláható.");
+*/
+
+/*
+ * Írjon programot, mely megszámolja, hogy a beírt mondatban hány darab megadott
+ * betű van!
+*/
+
+/*
+// Bekérünk egy mondatot
+string str = ""; // A mondat
+
+Console.Write("Írjon be egy mondatot! ");
+str = Console.ReadLine();
+
+// Bekérünk egy mintát
+string strSample = "";
+
+Console.Write("Írja be a keresendő mintát (egy karakter)! ");
+strSample = Console.ReadLine();
+
+char sample = (char)strSample[0];
+
+// A bekért mondat és minta ellenőrzése
+Console.WriteLine("A beírt mondat: " + str);
+Console.WriteLine("A beírt minta: " + sample);
+
+// Az minta megszámlálása
+// Az minta száma
+int nSample = 0;
+for (int i = 0; i < str.Length; i++) // A "Length" tulajdonság a string hossza
+{ 
+    if (str[i] == sample)
+        nSample++; // nSample += 1; // nSample = nSample + 1;
+}
+
+// Az eredmény kiíratása
+Console.WriteLine("A beírt mondatban " + nSample + " " + sample + " taláható.");
+*/
+
+/*
+ * Az input szövegből távolítsa el a szóközöket!
+ */
+
+/*
+// Bekérünk egy szöveget
+Console.Write("Írjon be egy szöveget! ");
+string str = Console.ReadLine();
+
+// Itt tárolom el az eredményeket
+string result = str.Replace(' ', '_');
+
+// Kiírom az eredményt
+Console.WriteLine("A szóköz nélküli szöveg: " + result);
+*/
+
+/*
+ * Olvasson be egy mondatot és egy szót! Mondja meg, hogy a szó szerepel-e a mondatban!
+*/
+
+/*
+// Bekérünk egy mondatot
+Console.Write("Írjon be egy mondatot! ");
+string sentence = Console.ReadLine();
+
+// Bekérünk egy mintát
+Console.Write("Írja be a keresendő szót! ");
+string word = Console.ReadLine();
+
+// A bekért mondat és minta ellenőrzése
+Console.WriteLine("A beírt mondat:!" + sentence + "!");
+Console.WriteLine("A beírt szó:!" + word + "!");
+
+// Annak eldöntése, hogy szerepel-e benne a szó
+if (sentence.Contains(word)) // Mintát az Contains metódussal lehet keresni
+    Console.WriteLine("Az adott szó szerepel a mondatban.");
+else
+    Console.WriteLine("Az adott szó nem szerepel a mondatban.");
+*/
+
+/*
+ * A beolvasott mondatok kisbetűit alakítsa nagybetűsre, a nagybetűs karaktereket pedig kisbetűsre!
+*/
+
+/*
+// Bekérünk egy mondatot
+Console.Write("Írjon be egy mondatot! ");
+string sentence = Console.ReadLine();
+
+for (int i = 0; i < sentence.Length; i++)
+{
+    if (sentence[i] >= 'a' && sentence[i] <= 'z')// ( || (sentence[i] >= 223))
+        Console.Write(sentence[i].ToString().ToUpperInvariant());
+    else
+        Console.Write(sentence[i].ToString().ToLowerInvariant());
+}
+*/
+
+/*
+ * Két mondatról döntse el, hogy azonosak-e! Ha a kis és nagybetűk különböznek, a programnak akkor is megoldást kell adnia!
+ * 
+ */
