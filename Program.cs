29293e71bf384c2096d2a8df7835e300f7bee0ad
Dr. Gabor FACSKO <59848559+gfacsko@users.noreply.github.com>
2022-11-25 00:56:50 +0100

    Függvények a kódban

--- Program.cs
+++ Program.cs
@@ -1,145 +1,229 @@
-﻿// See https://aka.ms/new-console-template for more information
-//string line;
-
-//Console.WriteLine("Szia, Vilag!"); // kiírás konzol képernyőre (a Console oszály statikus WriteLine metódusa)
-
-/*line = Console.ReadLine(); // Sor beolvasása
-Console.WriteLine(line);  // A beolvasott sor kiírása
-
-Console.Write(line);
-
-Console.WriteLine("Nyomj le egy billentyűt!");
-Console.ReadKey();*/
-
-/*
-int n = 0;
-byte b = 0;
-string str = "szoveg";
-char c = 'c';
-bool l = false;
-
-Console.WriteLine(n);
-Console.WriteLine(b);
-
-Console.WriteLine(str);
-Console.WriteLine(str[3]); // A szamozas 0-tol kezdodik.
-Console.WriteLine(c);
-
-Console.WriteLine(l);
-
-Console.WriteLine(sizeof(int)); // Wikipédia
-
-int j, k, m;
-
-j = 1;
-k = 2;
-//m = 3;
-
-Console.WriteLine(j);
-Console.WriteLine(k);
-//Console.WriteLine(m);
-
-//float j = 1.2;
-
-*/
-
-/*string str = "vege";
-
-Console.WriteLine("eleje, " + str);*/
-
-//int n = "szoveg";
-//int n = 1.4;
-
-//int n = 137;
-
-/*Console.WriteLine(n % 42);
-
-Console.WriteLine(n == 43);
-Console.WriteLine(n > 43);
-Console.WriteLine(n < 43);
-Console.WriteLine(n <= 43);
-
-if (n > 42 && n == 137)
-    Console.WriteLine("Nagyobb");*/
-
-/*for (int i=0;i<n;i++)
-    Console.WriteLine(i);*/
-
-/*
-// Tomb merete
-const int nSzavak = 10; 
-
-// Szavak tomb deklaracio
-string[] szavak = new string[nSzavak] {"egy","ketto","harom","negy","ot","hat","het","nyolc","kilenc","tiz"};
-
-// Kiiratas
-for (int i = 0; i < nSzavak;i++)
-    Console.WriteLine(szavak[i]);
-*/
-
-/*
-// Elso 100 szam kiiratasa ciklussal
-const int n = 100;
-
-for (int i = 1; i <= n; i++)
-    Console.WriteLine(i.ToString());
-*/
-
-/*
+﻿/*
+ * Függvények
  * 
-// Az elso n szam paros, paratlan és 13-mal oszthato számainak összege
-const int n = 100;
-
-// Összeg
-int s = 0;
-
-for (int i = 0; i <= n; i++)
-{
-    // Páros számok összege
-    //if ((i % 2) == 0)
-    // Páratlan számok összege
-    // if ((i % 2) == 1)
-    // 13-mal osztható számok összge
-    if ((i % 13) == 0)
-    {
-        s = s + i;
-    };
-};
-
-// Eredmeny kiiratasa
-Console.WriteLine(s);
-*/
-
-// A while ciklus
-
-const int n = 100;
-
-int i = 0;
-
-while (i < n)
-{
-    i++;
-    Console.WriteLine(i.ToString());
-}
-
-Console.WriteLine("While ciklus vége.");
+ * Developed by Gabor FACSKO (facsko.gabor@uni-milton.hu)
+ * Milton Friedman University, Budapest, Hungary, 2022 
+ * 
+ * */
 
 /*
-// A do-while ciklus
-
-const int n = 100;
-
-int i = 0;
-
-do
+// Első függvény deklaráció: szöveg kiírás
+void elsoFuggveny(string s)
 {
-    i++;
-    Console.WriteLine(i.ToString());
+    // Kiírja a paraméterként kapott szöveget
+    Console.WriteLine(s);
 }
-while (i < n);
 
+// A függvény meghívása
+elsoFuggveny("Teszt.");
+*/
 
-Console.WriteLine("A do-while ciklus vége.");
+/*
+// Második függvény deklaráció: négyzetre emelés
+int masodikFuggveny(int x)
+{
+    x = x * x;
+
+    return x;
+}
+
+// Megadok egy változót
+int x = 42;
+
+// A változó értékének kiírása
+Console.WriteLine("A bemeneti érték: " +x);
+
+// A függvény eredményének kiírása
+Console.WriteLine("A kimeneti érték: " + masodikFuggveny(x));
 */
 
 
+/*
+// Szavak kiírása függvénnyel
+
+// Új függvény deklarációja
+void szavakKiirasa(string[] s)
+{
+    int N = s.Length;
+    // Szavak kiíratása ciklussal
+    for (int i = 0; i < N; i++)
+    {
+        Console.WriteLine(s[i]);
+    }
+}
+
+// Tömb deklarációja az eljárás teszteléséhez
+string[] szavak = new string[] { "egy", "kettő", "három", "négy", "öt", "hat", "hét", "nyolc", "kilenc", "tíz" };
+
+szavakKiirasa(szavak);
+
+string[] masszavak = {"yksi", "kaksi", "kolme", "neilja", "viisi", "kuusi" };
+
+szavakKiirasa(masszavak);
+*/
+
+/*
+// Párosság vizsgálat, logokai operátorok ---------------------------------
+bool oszthatoE(int n, int m)
+{
+    return (n % m == 0);
+}
+
+// Kiértékelés
+void kiertekel(bool l)
+{
+    // Kiértékelés
+    if (l)
+    {
+        // Több utasítást is végrehajt
+        Console.WriteLine("Igen. .)");
+    }
+    else
+    {
+        Console.WriteLine("Nem. :(");
+    }
+}
+
+// Teszt értékek
+int n = 137;
+int m = 2;
+
+// Ellenőrzés
+Console.WriteLine("A " + n + " osztható-e a(z) " + m + " számmal?");
+
+// Oszthatóság vizsgálata
+bool lOszthato = oszthatoE(m, n);
+
+// Kiértékelés
+kiertekel(lOszthato);
+
+// Egyben a kettő.
+kiertekel(oszthatoE(m, n));
+*/
+
+
+/*
+// Másodfokú egyenletet megoldó program
+
+// Beolvas egy racionális számot
+double readDouble()
+{
+    // Beolvas egy sort
+    string line = Console.ReadLine();
+    // Átalakítja törté és visszaadja a számot
+    return Convert.ToDouble(line);
+}
+
+// Kiszámolja a diszkriminánst
+double diszkriminans(double a, double b, double c)
+{ 
+    return Math.Pow(b, 2) - 4 * a * c;
+}
+
+// A másodfokú egyelet megoldása
+void masodfokuEgyenlet(double a, double b, double c)
+{
+    // Diszkrimináns számítása
+    double d = diszkriminans(a, b, c);
+    Console.WriteLine("A diszkrimináns: " + d);
+
+    // Kiértékelés a diszkrimináns alapján
+    switch (d)
+    {
+        case > 0:
+            Console.WriteLine("D > 0, az egyenletnek így két megoldása van.");
+            double x1 = (-1 * b - Math.Sqrt(d)) / (2 * a);
+            double x2 = (-1 * b + Math.Sqrt(d)) / (2 * a);
+            Console.WriteLine("Az egyenlet megoldásai: x1 = " + x1 + " x2 = " + x2);
+            break;
+        case < 0:
+            Console.WriteLine("Az egyenletnek nincsen megoldása a valós számok körében.");
+            break;
+        default:
+            Console.WriteLine("D = 0, az egyenletnek így csak egy megoldása van.");
+            double x = (-1 * b) / (2 * a);
+            Console.WriteLine("Az egyenlet megoldása: x = " + x);
+            break;
+    }
+}
+
+// Állandók bekérése
+Console.WriteLine("A másodfokú egyenlet általános alakja: a x^2 + b x + c = 0 ");
+// a állandó
+Console.Write("a = ");
+double a = readDouble();
+// b állandó
+Console.Write("b = ");
+double b = readDouble();
+// c állandó
+Console.Write("c = ");
+double c = readDouble();
+
+// Ellenőrzés
+Console.WriteLine("A megadott egyenlet: " + a + " x^2 + " + b + " x + " + c);
+
+// Megoldja a másodfokú egyenletet
+masodfokuEgyenlet(a, b, c);
+*/
+
+// Összegző program
+
+// Véletlen számokkal feltöltötte tömb elemeinek összege
+
+internal class Program
+{
+    // A tömb elemeinek a kiírása
+    public static void tombKiiras(int[] array)
+    {
+        Console.Write("A tömb elemei: ");
+        for (int i = 0; i < array.Length; i++)
+            Console.Write(array[i] + " ");
+    }
+
+    private static void Main(string[] args)
+    {
+        // Véletlen számok generálása függvény
+        int[] tombFeltoltes(int n) // n: a tömb elemeinek a száma
+        {
+            // Tömb
+            int[] array = new int[n];
+            // Véletlen szám
+            Random rnd = new Random();
+            // Tömb feltöltése véletlen számokkal
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = rnd.Next(0, 255);
+            }
+
+            return array;
+        }
+
+      
+
+        // A tömb elemeinek az összegének a meghatározása
+        int tombOsszege(int[] array)
+        {
+            int osszeg = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                osszeg += array[i];
+            }
+
+            return osszeg;
+        }
+
+        // -------------------------------------------------------------------------
+
+        // A tömb eleminek a száma
+        const int N = 20;
+
+        // Feltölötöm a tömböt véletlenszámokkal
+        int[] array = tombFeltoltes(N);
+
+        // A tömb elemeinek a kiírása
+        tombKiiras(array);
+
+        // A tömb elemeinek az összegének a meghatározása és kiírása
+        Console.WriteLine("\nA tömb elemeinek összege: " + tombOsszege(array));
+    }
+}
